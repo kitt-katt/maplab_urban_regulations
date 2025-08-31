d82f8326ea5b343296faff24d12a0ebeff90fef4
kitt-katt <d15c0nn3ct.post@gmail.com>
2025-08-31 21:04:58 +0300

    Добавлена документация по api

--- app/urban_project/settings.py
+++ app/urban_project/settings.py
@@ -43,6 +43,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tep_api',
+    'drf_spectacular',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +132,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    'TITLE': 'Urban Regulations API',
+    'DESCRIPTION': 'API для получения нормативной документации по градостроительству',
+    'VERSION': '1.0.0',
+    'SERVE_INCLUDE_SCHEMA': False,
+}
--- app/urban_project/urls.py
+++ app/urban_project/urls.py
@@ -1,8 +1,16 @@
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView, SpectacularRedocView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # Подключаем маршруты нашего приложения tep_api
     path('api/v1/', include('tep_api.urls')),
+    # Маршруты для документации OpenAPI
+    path('api/v1/schema/', SpectacularAPIView.as_view(), name='schema'),
+    # Swagger UI:
+    path('api/v1/schema/swagger-ui/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    # Redoc:
+    path('api/v1/schema/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
 ]
