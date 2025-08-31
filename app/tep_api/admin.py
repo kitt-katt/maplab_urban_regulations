1974db201b8cc6e71a4d0878c327a89ca3f09df7
kitt-katt <d15c0nn3ct.post@gmail.com>
2025-08-31 22:54:52 +0300

    Явное указание для Django, какие модели нужны в админке

--- app/tep_api/admin.py
+++ app/tep_api/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Region, Parameter, ParameterValue
 
-# Register your models here.
+
+# Класс для встраивания значений на страницу редактирования Региона
+class ParameterValueInline(admin.TabularInline):
+    model = ParameterValue
+    extra = 1  # Показывать одну пустую строку для добавления
+
+
+# Регистрируем модель Региона и встраиваем в неё управление значениями
+@admin.register(Region)
+class RegionAdmin(admin.ModelAdmin):
+    list_display = ('region_name', 'region_id')
+    inlines = [ParameterValueInline]
+
+
+# Регистрируем модель Параметра для отдельного управления
+@admin.register(Parameter)
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('name', 'units', 'description')
+    search_fields = ('name', 'description') # Добавляем поиск для удобства
